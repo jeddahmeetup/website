e50cc23b0033eb08a3df3f40e69a0a9306d274c2
Samy Alzahrani <samyalzahrani@Samys-MacBook-Air.local>
2023-03-24 13:02:19 +0300

    Revert "wip"
    This reverts commit 50a1e16737258d3e565ebce4c3bba78ecdec60c7.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,4 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
-import vercel from "@astrojs/vercel/edge";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel(),
-});
+export default defineConfig({});
--- src/auth/adminClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../../types/supabase";
-
-export const adminClient = createClient<Database>(
-  import.meta.env.PUBLIC_SUPABASE_URL,
-  import.meta.env.SUPABASE_SECRET
-);
